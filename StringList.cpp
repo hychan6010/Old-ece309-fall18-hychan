2f105eda345d48ad832286c0bd9ec1ef1ba1cc62
hychan6010 <42948014+hychan6010@users.noreply.github.com>
2018-09-03 17:37:02 -0400

    Create StringList.cpp

---
+++ StringList.cpp
@@ -0,0 +1,31 @@
+class ListNode {
+private:
+  Item item;
+  ListNode *next;
+ public:
+  ListNode(Item a)
+    { item = a; next=NULL }
+  ListNode* getNext() 
+  {
+    return next;
+  }
+  void setNext(ListNode *n) 
+  {
+  next = n;
+  }
+  Item getItem() 
+  {
+  return item;
+  }
+ };
+ 
+class List {
+private:
+    ListNode *head;
+    ListNode *tail;
+ public:
+    List();
+    void append(Item a);
+    bool remove(Item &a);
+    bool empty();
+    };
